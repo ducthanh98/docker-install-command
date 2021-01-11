8bc76f1183db4f13bff7ae780246bd8b644d7096
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2021-01-11 16:46:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,6 @@ sudo apt-get install zsh -y
 sudo curl -L http://install.ohmyz.sh | sh
 sudo apt install git -y
 
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+
