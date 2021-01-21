9aa82209d28734f65f973482732824793195f2ab
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2021-01-21 22:05:00 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
 sudo apt-get install zsh -y
 sudo curl -L http://install.ohmyz.sh | sh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 sudo apt install git -y
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
@@ -11,6 +15,8 @@ sudo npm install -g @vue/cli
 sudo apt install docker.io
 sudo chmod 666 /var/run/docker.sock
 
-sudo apt-get update && sudo apt-get install ibus-bamboo –y
+sudo apt-get install ibus-unikey -y
 
+docker run -d --network=host --name=redis redis
+docker run --name rabbitmq -d --network=host -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest  bitnami/rabbitmq:latest
 
