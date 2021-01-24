f633d7d0c7a6b1c99b58e37ca8390e30cf88af8e
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2021-01-24 19:18:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-
+sudo apt-get install -y curl
 sudo apt-get install -y tmux
 sudo apt -y install python3
 sudo apt install -y python3-pip
