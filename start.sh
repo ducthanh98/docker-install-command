67bda38064eb67ae1b5b76bded837f484278bf8e
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2021-01-24 19:29:38 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,6 +11,8 @@ pip3 install virtualenv
 sudo apt-get install -y zsh
 sudo curl -L http://install.ohmyz.sh | sh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
 
 
