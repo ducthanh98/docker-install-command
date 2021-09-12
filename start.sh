bb302cb660cb9f10de7161e3f4fb9417249d043d
Lê Đức Thanh <38995368+ducthanh98@users.noreply.github.com>
2021-09-12 18:33:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -24,7 +24,7 @@ chsh -s $(which zsh)
 sudo apt install -y git
 git config --global credential.helper store
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt -y install nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
